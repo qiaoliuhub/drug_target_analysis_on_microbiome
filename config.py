9226c19c8106db1e58bed180fa01f1f37000b002
QiaoLiu <smileqiao1988@yahoo.com>
2019-09-26 19:27:05 -0400

    add new file

---
+++ config.py
@@ -0,0 +1,12 @@
+beneficial_species_file = "Data/ori_beneficial.csv"
+pathogen_species_file = "Data/ori_pathogen.csv"
+activation = "Data/sample_activation.csv"
+inhibition = "Data/sample_inhibition.csv"
+specie_id_map = "Data/species_id_map.csv"
+
+path_acti_bene_acti = "Data/result/path_acti_bene_acti.csv"
+path_acti_bene_inhi = "Data/result/path_acti_bene_inhi.csv"
+path_inhi_bene_acti = "Data/result/path_inhi_bene_acti.csv"
+path_inhi_bene_inhi = "Data/result/path_inhi_bene_inhi.csv"
+
+path_affe_bene_affe = "Data/result/path_affe_bene_affe.csv"
---
+++ select_drugs.py
@@ -0,0 +1,37 @@
+#! /usr/env/bin python
+import config
+import pandas as pd
+
+### drugs inhibit "bad"
+### drugs inhibit "good"
+### drugs activate "bad"
+### drugs activate "good"
+if __name__ == "__main__":
+
+    beneficial_species = pd.read_csv(config.beneficial_species_file)
+    pathogen_species = pd.read_csv(config.pathogen_species_file)
+    activation = pd.read_csv(config.activation, header = None)
+    inhibition = pd.read_csv(config.inhibition, header = None)
+    specie_name_id = pd.read_csv(config.specie_id_map)
+
+    pathogen_species_id = set(pathogen_species['id'])
+    pathogen_taxon_id = set(specie_name_id[specie_name_id['id'].isin(pathogen_species_id)]['taxon_id'])
+    beneficial_species_id = set(beneficial_species['id'])
+    beneficial_taxon_id = set(specie_name_id[specie_name_id['id'].isin(beneficial_species_id)]['taxon_id'])
+
+    path_acti = activation[activation[6].isin(pathogen_taxon_id)]
+    bene_acti = activation[activation[6].isin(beneficial_taxon_id)]
+    path_inhi = inhibition[inhibition[6].isin(pathogen_taxon_id)]
+    bene_inhi = inhibition[inhibition[6].isin(beneficial_taxon_id)]
+
+    output_file = iter([config.path_acti_bene_acti, config.path_acti_bene_inhi, config.path_inhi_bene_acti, config.path_inhi_bene_inhi])
+    path_affe, bene_affe = set(), set()
+    for path in [path_acti, path_inhi]:
+        for bene in [bene_acti, bene_inhi]:
+            path_chem = set(x for x in (set(path[0]) | set(path[1])) if x.startswith('CID'))
+            path_affe |= path_chem
+            bene_chem = set(x for x in (set(bene[0]) | set(bene[1])) if x.startswith('CID'))
+            bene_affe |= bene_chem
+            pd.DataFrame({'CIDs': list(path_chem - bene_chem)}).to_csv(next(output_file), index = False)
+
+    pd.DataFrame({'CIDs': list(path_affe-bene_affe)}).to_csv(config.path_affe_bene_affe, index = False)
