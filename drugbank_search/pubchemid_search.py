c5311e0f74266db71800596e1ecd61d9b7134a94
Qiao Liu <qliu@saturn.cs.hunter.cuny.edu>
2019-10-01 19:39:00 +0000

    debug_sa

--- drugbank_search/pubchemid_search.py
+++ drugbank_search/pubchemid_search.py
@@ -4,9 +4,8 @@ import xml.etree.ElementTree as ET
 from drugbankid_search import pubchemid2infos
 if __name__ == "__main__":
 
-    with open(sys.argv[1]) as drugs_file:
-        drugs = drugs_file.readline().split(',')
-
+    drugs = pd.read_csv(sys.argv[1])
+    drugs = list(drugs['CIDs'].str[4:])
     drugs = list(map(str.strip, drugs))
     parse_tree = ET.parse(sys.argv[2])
     root = parse_tree.getroot()
@@ -16,7 +15,7 @@ if __name__ == "__main__":
     for i, drug in enumerate(drugs):
         drugs_df.loc[i] = pubchemid2infos(root, drug)
 
-    unfound = drugs_df[drugs_df['Name'].isnull()]['DrugBank ID']
+    unfound = drugs_df[drugs_df['Name'].isnull()]['PubChem Compound ID']
     with open(sys.argv[4], 'w+') as unfound_file:
         unfound_file.write(','.join(list(unfound)))
     drugs_df.to_csv(sys.argv[3], index=False)
