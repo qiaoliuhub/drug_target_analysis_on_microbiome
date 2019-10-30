c04247ffff1f20f15c4e4c89d7439f45a86a4ee9
Qiao Liu <qliu@saturn.cs.hunter.cuny.edu>
2019-10-30 14:04:43 +0000

    add one results

--- fasta_blast/config/aligned_protein_in_pdb_config.py
+++ fasta_blast/config/aligned_protein_in_pdb_config.py
@@ -1,8 +1,8 @@
 # folder to find all the xml result
-input_xml_folder = "/data/saturn/a/qliu/blast_chembl/blast_results"
+input_xml_folder = "/data/saturn/a/qliu/blast_pdb/blast_chembl_2019102918/blast_results"
 
 # folder to save all the distribution result
-result_folder = "/data/saturn/a/qliu/blast_chembl/distributions"
+result_folder = "/data/saturn/a/qliu/blast_pdb/blast_chembl_2019102918/distributions"
 
 # plots curves up to 0.1 ** maximum_evalue_index
 maximum_evalue_index = 200
